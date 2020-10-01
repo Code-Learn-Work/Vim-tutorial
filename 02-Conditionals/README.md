e267762fc05adbb5b353ba9595415fc7ddd1b579
Mayank Khanna <khanna98@outlook.com>
2020-10-01 16:30:44 +0530

    Conditionals

---
+++ 02-Conditionals/README.md
@@ -0,0 +1,6 @@
+# Conditionals
+
+This is a repo where you can create and practice conditionals in various languages.
+
+The following language code has been added to far:
+1. bash
---
+++ 02-Conditionals/if-else.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to conditionals"
+
+todayDate=$(date +%d)
+today=$(date)
+echo "Today is $today."
+if [ $todayDate -ge 15 ]
+then
+  echo "Congrats half the month is over !"
+else
+  echo "Come on work hard. Its not even the half of the month"
+fi
